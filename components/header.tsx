035bab187130c35920af172fb3878be30c712d75
Danial Hasan <danialhasan18@gmail.com>
2026-02-20 14:10:52 +0100

    add product modal
    when you click add product it will open up a form.

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,31 @@
-export default function Header() {
-  return (
-    <header className="flex justify-between px-6 py-4 bg-white border-gray-200 border-2 rounded-tr-lg">
-      <div>
-        <h2 className="text-2xl font-bold">Product management</h2>
-        <span className="text-black/70">Manage your store inventory</span>
-      </div>
+"use client";
 
-      <button className="bg-purple-700 px-5 my-2 text-white rounded-sm hover:bg-purple-800 active:bg-purple-900">
-        + Add product
-      </button>
-    </header>
+import { useState } from "react";
+import AddProductModal from "./AddProductModal";
+
+export default function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <header className="flex justify-between px-6 py-4 bg-white border-gray-200 border-2 rounded-tr-lg">
+        <div>
+          <h2 className="text-2xl font-bold">Product management</h2>
+          <span className="text-black/70">Manage your store inventory</span>
+        </div>
+
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-purple-700 px-5 my-2 text-white rounded-sm hover:bg-purple-800 active:bg-purple-900"
+        >
+          + Add product
+        </button>
+      </header>
+
+      <AddProductModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 }
