d8e91345fa7502636d0f1a34d4f7d21935e976a2
ThomasJ99 <xmaslink1999@gmail.com>
2026-02-20 12:09:41 +0100

    Styled links with current page being already selected - hardcoded atm
    rest of links have hover and active effect
    
    adjusted spacing between link elements
    
    tweaked admin panel font and h1 group spacing
    
    changed color of border to match with the header
    
    removed placeholder colors

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,22 +1,27 @@
 export default function Sidebar() {
   return (
-    <div className="bg-white fixed top-0 left-0 z-99 w-64 h-full flex flex-col border-e">
+    <div className="bg-white fixed top-0 left-0 z-99 w-64 h-full flex flex-col border-e border-gray-200">
       <aside id="default-sidebar" className="" aria-label="Sidebar">
-        <div className="h-full py-4 overflow-y-auto">
+        <div className="h-full py-4">
           <ul className="space-y-2 font-medium">
-            <li className="border-b-2 mt-2 mb-5">
-              <a href="#" className=" bg-amber-100">
+            <li className="border-b-2 border-gray-200 mt-2 mb-5 ps-5">
+              <a href="#" className="">
                 <h1 className="text-2xl font-bold text-pretty leading-tight px-3">
                   Webbutiken
                 </h1>
-                <span className="px-3 text-black/70">Admin panel</span>
+                <span className="px-3 text-black/70 font-light text-sm">
+                  Admin panel
+                </span>
               </a>
             </li>
 
             <nav>
-              <ul>
+              <ul className="ps-5 me-4 space-y-2">
                 <li>
-                  <a href="#" className="flex items-center px-2 py-1.5">
+                  <a
+                    href="#"
+                    className="flex items-center px-2 py-2 gap-4 bg-purple-700 rounded-sm text-white hover:bg-purple-800 active:bg-purple-900"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -32,12 +37,15 @@ export default function Sidebar() {
                       <path d="M16.76 3a2 2 0 0 1 1.8 1.1l2.23 4.479a2 2 0 0 1 .21.891V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9.472a2 2 0 0 1 .211-.894L5.45 4.1A2 2 0 0 1 7.24 3z" />
                       <path d="M3.054 9.013h17.893" />
                     </svg>
-                    <span className="flex-1 ms-3">Products</span>
+                    <span className="">Products</span>
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" className="flex items-center px-2 py-1.5 group">
+                  <a
+                    href="#"
+                    className="flex items-center px-2 py-2 gap-4 rounded-sm hover:text-white hover:bg-purple-700 active:bg-purple-800"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -52,12 +60,15 @@ export default function Sidebar() {
                       <path d="M3 3v16a2 2 0 0 0 2 2h16" />
                       <path d="m19 9-5 5-4-4-3 3" />
                     </svg>
-                    <span className="flex-1 ms-3">Analytics</span>
+                    <span className="">Analytics</span>
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" className="flex items-center px-2 py-1.5">
+                  <a
+                    href="#"
+                    className="flex items-center px-2 py-2 gap-4 rounded-sm hover:text-white hover:bg-purple-700 active:bg-purple-800"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -73,12 +84,15 @@ export default function Sidebar() {
                       <circle cx="19" cy="21" r="1" />
                       <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
                     </svg>
-                    <span className="flex-1 ms-3">Orders</span>
+                    <span className="">Orders</span>
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" className="flex items-center px-2 py-1.5">
+                  <a
+                    href="#"
+                    className="flex items-center px-2 py-2 gap-4 rounded-sm hover:text-white hover:bg-purple-700 active:bg-purple-800"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -95,12 +109,15 @@ export default function Sidebar() {
                       <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                       <circle cx="9" cy="7" r="4" />
                     </svg>{" "}
-                    <span className="flex-1 ms-3">Customers</span>
+                    <span className="">Customers</span>
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" className="flex items-center px-2 py-1.5">
+                  <a
+                    href="#"
+                    className="flex items-center px-2 py-2 gap-4 rounded-md hover:text-white hover:bg-purple-700 active:bg-purple-800"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -115,7 +132,7 @@ export default function Sidebar() {
                       <path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
                       <circle cx="12" cy="12" r="3" />
                     </svg>{" "}
-                    <span className="flex-1 ms-3">Settings</span>
+                    <span className="">Settings</span>
                   </a>
                 </li>
               </ul>
@@ -125,7 +142,7 @@ export default function Sidebar() {
       </aside>
 
       {/* Login bar */}
-      <section className="border-t-2 mt-auto">
+      <section className="border-t-2 border-gray-200 mt-auto">
         {/* IMAGE */}
         <div className="flex gap align-items">
           <img src="https://i.pravatar.cc/54" alt="" />
